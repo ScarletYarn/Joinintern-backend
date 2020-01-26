41d195aac4a622c387a8bea998a3341cffbd219b
Brunnhild <10175101259@stu.ecnu.edu.cn>
2020-01-26 14:42:41 +0800

    Fxied the bug in test.

--- src/test/java/com/joininterngroup/joinintern/JoininternApplicationTests.java
+++ src/test/java/com/joininterngroup/joinintern/JoininternApplicationTests.java
@@ -3,7 +3,7 @@ package com.joininterngroup.joinintern;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class JoininternApplicationTests {
 
 	@Test
